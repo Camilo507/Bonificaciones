a05e4dcfa006ded4181725d25842ed98034038ff
Miguel Vargas <miguelaglvs@gmail.com>
2023-02-17 18:59:16 -0500

    Se agrego una suma

--- ayuda1/src/Ayuda1.java
+++ ayuda1/src/Ayuda1.java
@@ -1,6 +1,8 @@
 public class Ayuda1 {
     public static void main(String[] args) {
         System.out.println("hola camilo");
-        int a;
+        int a = 1, b = 2, suma;
+        suma = a+b;
+        System.out.println(suma);
     }  
 }
